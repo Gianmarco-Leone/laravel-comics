dde3d01fca6b48d6d7c13aabb70fe1f3d3605adf
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-03-29 09:17:27 +0200

    add main nav

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -31,4 +31,56 @@
       </div>
     </div>
 </div>
+
+<div class="main-nav">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="my-card">
+          <img
+            src="./img/buy-comics-digital-comics.png"
+            alt="Digital comics"
+          />
+          <span>DIGITAL COMICS</span>
+        </div>
+      </div>
+      <div class="col">
+        <div class="my-card">
+          <img
+            src="./img/buy-comics-merchandise.png"
+            alt="Digital comics"
+          />
+          <span>DC MERCHANDISE</span>
+        </div>
+      </div>
+      <div class="col">
+        <div class="my-card">
+          <img
+            src="./img/buy-comics-subscriptions.png"
+            alt="Digital comics"
+          />
+          <span>SUBSCRIPTION</span>
+        </div>
+      </div>
+      <div class="col">
+        <div class="my-card">
+          <img
+            src="./img/buy-comics-shop-locator.png"
+            alt="Digital comics"
+          />
+          <span>COMIC SHOP LOCATOR</span>
+        </div>
+      </div>
+      <div class="col">
+        <div class="my-card">
+          <img
+            src="./img/buy-dc-power-visa.svg"
+            alt="Digital comics"
+          />
+          <span>DC POWER VISA</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
